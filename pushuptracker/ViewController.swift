097f9df454e1ace0c671838cb15426277ead7e91
JoshuaLoveCoding <anqi6591@gwu.edu>
2018-10-30 10:45:37 -0400

    First commit

--- pushuptracker/ViewController.swift
+++ pushuptracker/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pushupRecordLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+ 
+        pushupRecordLabel.text = "Hello World"
+        
+        
     }
 
 
